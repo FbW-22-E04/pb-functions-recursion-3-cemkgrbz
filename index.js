0aea44c30a00d45a18403fe4bef6098624de4645
Cem <cemkgrbz@gmail.com>
2022-08-22 15:51:21 +0200

    added js file

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+const string="hello world"
+
+function reverse(str){
+    if(str.length === 0){
+        return str;
+    } else {
+        return reverse(str.slice(1)) + str[0]
+    }
+}
+
+console.log(reverse(string));  // return dlrow olleh
